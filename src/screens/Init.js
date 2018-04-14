c853f066e333d8edf128e02169a7e5baa48411a2
Van Pho Nguyen <vanpho02@gmail.com>
2018-04-14 14:19:45 +0700

    Init

--- src/screens/Init.js
+++ src/screens/Init.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
 
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 export class Init extends Component {
     render() {
         return (
-            <View>
-                <Text>Init Component</Text>
+            <View style={styles.container}>
+                <Text style={styles.title}>
+                    Init Component
+                </Text>
+                <TouchableOpacity style={styles.buttonContainer}>
+                    <Text style={styles.buttonText}>Show</Text>
+                </TouchableOpacity>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'lightblue',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    title: {
+        color: 'gray',
+        fontWeight: 'bold',
+        fontSize: 20
+    },
+    buttonContainer: {
+        backgroundColor: '#25C73A',
+        padding: 10,
+        borderRadius: 5,
+        borderColor: '#3EA552',
+        borderWidth: 1,
+        margin: 10
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: 'bold'
+    }
+});
