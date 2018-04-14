e5ac0012cc07e3355a2cfd0eec09ccc36368c071
Van Pho Nguyen <vanpho02@gmail.com>
2018-04-14 14:02:29 +0700

    Init Component

---
+++ App.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { Init } from './src/screens/Init';
+
+export default class App extends Component {
+  render() {
+    return <Init />;
+  }
+}
---
+++ src/screens/Init.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import { View, Text, StyleSheet } from 'react-native';
+
+export class Init extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Init Component</Text>
+            </View>
+        );
+    }
+}
