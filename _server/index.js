9fe41c19f299e6efc87fd7c040b677f3194b178b
Van Pho Nguyen <vanpho02@gmail.com>
2018-04-21 14:51:42 +0700

    index.js

--- _server/index.js
+++ _server/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const { json } = require('body-parser');
 
 const app = express();
 
-// app.get('/signin/:email/:password', (req, res) => {
-//     console.log(req.params);
-//     res.send('Hello.');
-// });
+app.use(json());
+
+app.get('/signin/:email/:password', (req, res) => {
+    console.log(req.params);
+    res.send('Hello.');
+});
 
 app.post('/signin', (req, res) => {
+    console.log(req.body);
     res.send({ success: true, message: 'Success!' });
 });
 
--- src/nav/Stack.js
+++ src/nav/Stack.js
@@ -3,14 +3,6 @@ import { Account } from './Account';
 import { SignIn } from './SignIn';
 import { StackNavigator } from 'react-navigation';
 
-// export class Stack extends Component {
-//     render() {
-//         return (
-//             <Account />
-//         );
-//     }
-// }
-
 export const Stack = StackNavigator({
     SignIn: { screen: SignIn },
     Account: { screen: Account },
