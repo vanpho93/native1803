ed0c8455d94e5729f0376808b2d718bfac1b4a79
Van Pho Nguyen <vanpho02@gmail.com>
2018-04-21 14:13:49 +0700

    Drawer

--- App.js
+++ App.js
@@ -5,11 +5,12 @@ import { List } from './src/screens/List';
 import { Weather } from './src/weather/Weather';
 import { Stack } from './src/nav/Stack';
 import { TabNav } from './src/nav/TabNav';
+import { Drawer } from './src/nav/Drawer';
 
 StatusBar.setHidden(true);
 
 export default class App extends Component {
   render() {
-    return <TabNav />;
+    return <Drawer />;
   }
 }
---
+++ src/nav/Drawer.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { View, StyleSheet, Text, Image } from 'react-native';
+import { DrawerNavigator } from 'react-navigation';
+import { Button } from '../shared/Button';
+
+class Account extends Component {
+    render() {
+        return (
+            <View style={styles.accountContainer}>
+                <Text style={styles.title}>Account Component</Text>
+                <Button
+                    title="Log out"
+                    type="warning"
+                />
+            </View>
+        );
+    }
+}
+
+class Menu extends Component {
+    render() {
+        return (
+            <View style={styles.signInContainer}>
+                <Text style={styles.title}>Menu Component</Text>
+                <Button
+                    title="Login"
+                    type="success"
+                />
+            </View>
+        );
+    }
+}
+
+export const Drawer = DrawerNavigator({
+    Account: { screen: Account }
+});
+
+const styles = StyleSheet.create({
+    aboutContainer: {
+        backgroundColor: '#EF4F21',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    accountContainer: {
+        backgroundColor: 'lightblue',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    signInContainer: {
+        backgroundColor: '#C4B7D7',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    title: {
+        color: 'gray',
+        fontWeight: 'bold',
+        fontSize: 20
+    }
+});
+
